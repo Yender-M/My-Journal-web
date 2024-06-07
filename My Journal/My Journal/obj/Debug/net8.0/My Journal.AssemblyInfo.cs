43c717adca36612c11860b6ce98601aae53063f9
cinthia <cinthiaduarte013@gmail.com>
2024-06-06 21:19:31 -0600

    Multi ingreso cambios

--- My Journal/My Journal/obj/Debug/net8.0/My Journal.AssemblyInfo.cs
+++ My Journal/My Journal/obj/Debug/net8.0/My Journal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My Journal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c9470036b9978fa90aa8f8c7f6722a5bbc0a608")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8fe7853fa6a040b48264185c444de19d5cbc9a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("My Journal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My Journal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
