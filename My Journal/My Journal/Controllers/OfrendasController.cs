d7311aed3f7aa48a5bf64824e4ce7e191c6dfe85
Yender-M <114435332+Yender-M@users.noreply.github.com>
2024-06-06 23:01:24 -0600

    Insersion Multiple

--- My Journal/My Journal/Controllers/OfrendasController.cs
+++ My Journal/My Journal/Controllers/OfrendasController.cs
@@ -106,28 +106,69 @@ namespace My_Journal.Controllers
         //    }
         //}
 
-        public ActionResult Create(OfrendaListViewModel ofrendaListViewModel)
+        [HttpPost]
+        [HttpPost]
+        public ActionResult Create(List<int> OfrendaCategoria,List<string> Descripcion,List<decimal> Cantidad,List<int> Divisa,List<decimal> TasaCambio,List<DateTime> Fecha)
         {
             try
             {
-                MantOfrendaCategoria mant = new MantOfrendaCategoria();
-                var categorias = mant.Getlistado();
-                var categoriasSelectList = new SelectList(categorias, "IdCatOfrenda", "Nombre");
-                ViewBag.ListadoOfrendasCategorias = categoriasSelectList;
-
-                foreach (var ofrendaViewModel in ofrendaListViewModel.Ofrendas)
+                // Validar que todas las listas tengan la misma longitud
+                if (OfrendaCategoria.Count == Descripcion.Count &&
+                    Descripcion.Count == Cantidad.Count &&
+                    Cantidad.Count == Divisa.Count &&
+                    Divisa.Count == TasaCambio.Count &&
+                    TasaCambio.Count == Fecha.Count)
                 {
-                    new MantOfrenda().Insertar(ofrendaViewModel);
-                }
+                    // Crear una lista para almacenar las ofrendas
+                    var ofrendas = new List<Ofrenda>();
 
-                return RedirectToAction("Index");
+                    // Iterar sobre las listas y crear objetos Ofrenda
+                    for (int i = 0; i < OfrendaCategoria.Count; i++)
+                    {
+                        var ofrenda = new Ofrenda
+                        {
+                            IdCatOfrenda = OfrendaCategoria[i],
+                            Descripcion = Descripcion[i],
+                            Cantidad = (double)Cantidad[i],
+                            Divisa = Divisa[i],
+                            TasaCambio = (double)TasaCambio[i],
+                            Fecha = Fecha[i]
+                        };
+                        ofrendas.Add(ofrenda);
+                    }
+
+                    // Insertar las ofrendas en la base de datos
+                    MantOfrenda mantOfrenda = new MantOfrenda();
+                    foreach (var ofrenda in ofrendas)
+                    {
+                        mantOfrenda.Insertar(ofrenda);
+                    }
+
+                    // Redirigir a la acción Index después de la inserción
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    throw new InvalidOperationException("Las listas proporcionadas tienen diferentes longitudes.");
+                }
             }
             catch (Exception ex)
             {
                 // Manejar la excepción según sea necesario
-                return View(ofrendaListViewModel);
+                // Cargar la lista de categorías para la vista en caso de error
+                MantOfrendaCategoria mantCategoria = new MantOfrendaCategoria();
+                var categorias = mantCategoria.Getlistado();
+                var categoriasSelectList = new SelectList(categorias, "IdCatOfrenda", "Nombre");
+                ViewBag.ListadoOfrendasCategorias = categoriasSelectList;
+
+                // Devolver la vista con los datos ingresados y el mensaje de error
+                ModelState.AddModelError("", "Ocurrió un error al guardar los datos: " + ex.Message);
+                return View();
             }
         }
+
+
+
 
         // GET: Ofrendas/Edit/5
         public async Task<IActionResult> Edit(int? id)
