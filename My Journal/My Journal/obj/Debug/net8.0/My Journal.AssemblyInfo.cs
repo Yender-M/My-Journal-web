393f55dd487d779d2d20638a2e530b8195df24a0
cinthia <cinthiaduarte013@gmail.com>
2024-06-11 21:13:09 -0600

    create miembro

--- My Journal/My Journal/obj/Debug/net8.0/My Journal.AssemblyInfo.cs
+++ My Journal/My Journal/obj/Debug/net8.0/My Journal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My Journal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf493b1e063dda3cf226ba540941d88a89c82a74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8a5090c3036bb720f327effefe2062e5bcaa8b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("My Journal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My Journal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
