1a85e8a2ffda71478552fd3c2607156b2b0db09a
cinthia <cinthiaduarte013@gmail.com>
2025-06-17 21:42:22 -0600

    arreglo datatables

---
+++ My Journal/My Journal/Controllers/OfrendasCategoriasCreateController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using My_Journal.Models.OfrendaCategoria;
+using My_Journal;
+
+[Route("OfrendasCategoriasCreate")]
+public class OfrendasCategoriasCreateController : Controller
+{
+    private readonly CbnIglesiaContext _context;
+
+    public OfrendasCategoriasCreateController(CbnIglesiaContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View("Index"); // o simplemente View();
+    }
+
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Index(List<string> Nombre, List<string> Descripcion)
+    {
+        if (Nombre.Count != Descripcion.Count)
+        {
+            ModelState.AddModelError("", "Los nombres y descripciones no coinciden.");
+            return View(new OfrendasCategoria());
+        }
+
+        try
+        {
+            for (int i = 0; i < Nombre.Count; i++)
+            {
+                var categoria = new OfrendasCategoria
+                {
+                    Nombre = Nombre[i],
+                    Descripcion = Descripcion[i],
+                };
+                _context.OfrendasCategorias.Add(categoria);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "OfrendasCategorias");
+        }
+        catch (Exception ex)
+        {
+            ModelState.AddModelError("", $"Error al guardar: {ex.Message}");
+            return View(new OfrendasCategoria());
+        }
+    }
+}
--- My Journal/My Journal/obj/Debug/net8.0/My Journal.AssemblyInfo.cs
+++ My Journal/My Journal/obj/Debug/net8.0/My Journal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My Journal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f2b69586aa8ca310eca632f6213651f25141548")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e863b5581803261a61157698d3f7a3a38b25021a")]
 [assembly: System.Reflection.AssemblyProductAttribute("My Journal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My Journal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
